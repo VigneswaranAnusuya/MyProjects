2a225bb59e83ad04791735c2af7bb6f15b9ca7a4
VigneswaranAnusuya <100022442+VigneswaranAnusuya@users.noreply.github.com>
2022-03-21 20:57:01 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,11 +1,12 @@
 var home  = new function(){
     this.arrAll = [
+        {"name":"Vigneswaran anusuya (LinkedIn)","link":"https://www.linkedin.com/in/vigneswaran-anusuyadevi"},
         {"name":"10 Sec Math Game (My Project)","link":"https://vigneswarananusuya.github.io/MyProjects/Games/MathGame/"},
         {"name":"JavaScript Certificate (HackerRank)","link":"https://www.hackerrank.com/certificates/86be58e519b3"},
         {"name":"Java Certificate (HackerRank)","link":"https://www.hackerrank.com/certificates/fc61b3a47a6a"},
         {"name":"Cutshort Certified Java - Advanced","link":"https://cutshort.io/certificate/63442"}
     ];
-    this.boxString='<div class="box" link="{{link}}"> <div class="info"> <div class="center name">{{name}}</div> </div> <div class="link"> <div class="linkVal">Copy</div> </div> </div>';
+    this.boxString='<div class="info"> <div class="center name">{{name}}</div> </div> <div class="link"> <div class="linkVal">Copy</div> </div>';
     this.regexString=[{"title":"name","regex":"{{name}}"},{"title":"link","regex":"{{link}}"}];
     this.currArr=[];
 
@@ -31,21 +32,20 @@ var home  = new function(){
         var parent = $("#tabs");
         parent.empty();
         arr.forEach(function(val){
-            var temp =home.boxString;var htmlObject;
+            var temp =home.boxString;var htmlObject;var link;
             home.regexString.forEach(function(v){
                 temp = temp.replace(v["regex"],val[v["title"]]);
-                htmlObject = document.createElement('div');
-                htmlObject.innerHTML = temp;
-                $(htmlObject).find('.linkVal').on('click',home.copyToClip);
-                $(htmlObject).find('.info').on('click',home.openInTab);
             });
+            htmlObject = $(document.createElement('div'));
+            htmlObject.html(temp).addClass('box').attr('link',val['link']);
+            htmlObject.find('.linkVal').on('click',home.copyToClip);
+            htmlObject.find('.info').on('click',home.openInTab);
             parent.append(htmlObject);
         });
     }
 
     this.init = function(){
         this.buildBoxes(this.arrAll);
-        //jQuery("#tabs").on({"click":home.processBoxClick});
     }
 
 }
