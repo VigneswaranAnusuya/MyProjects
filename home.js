04e9ed8bbbc821b3cd53a4ab4c9c5e07ff0a105a
VigneswaranAnusuya <100022442+VigneswaranAnusuya@users.noreply.github.com>
2022-03-27 21:28:35 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,34 +1,31 @@
-var home  = new function(){
+var home  = new function()
+{
     this.arrAll = [
-        {"name":"Vigneswaran anusuya (LinkedIn)","link":"https://www.linkedin.com/in/vigneswaran-anusuyadevi"},
-        {"name":"10 Sec Math Game (My Project)","link":"https://vigneswarananusuya.github.io/MyProjects/Games/MathGame/"},
-        {"name":"JavaScript Certificate (HackerRank)","link":"https://www.hackerrank.com/certificates/86be58e519b3"},
-        {"name":"Java Certificate (HackerRank)","link":"https://www.hackerrank.com/certificates/fc61b3a47a6a"},
-        {"name":"Cutshort Certified Java - Advanced","link":"https://cutshort.io/certificate/63442"}
+        {"name":"Vigneswaran anusuya (LinkedIn)","link":"https://www.linkedin.com/in/vigneswaran-anusuyadevi","type":"Profile"},
+        {"name":"10 Sec Math Game (My Project)","link":"https://vigneswarananusuya.github.io/MyProjects/Games/MathGame/","type":"Project"},
+        {"name":"JavaScript Certificate (HackerRank)","link":"https://www.hackerrank.com/certificates/86be58e519b3","type":"Certificate"},
+        {"name":"Java Certificate (HackerRank)","link":"https://www.hackerrank.com/certificates/fc61b3a47a6a","type":"Certificate"},
+        {"name":"Cutshort Certified Java - Advanced","link":"https://cutshort.io/certificate/63442","type":"Certificate"}
     ];
-    this.boxString='<div class="info"> <div class="center name">{{name}}</div> </div> <div class="link"> <div class="linkVal">Copy</div> </div>';
-    this.regexString=[{"title":"name","regex":"{{name}}"},{"title":"link","regex":"{{link}}"}];
+    this.boxString='<div class="info"> <div class="center name">{{name}}</div> </div> <div class="type"> <div class="typeVal">{{type}}</div> </div>';
+    this.regexString=[{"title":"name","regex":"{{name}}"},{"title":"type","regex":"{{type}}"}];
     this.currArr=[];
 
-    this.processBoxClick = function(ev){
-        if(ev.target.id=="tabs"){
-                    console.log(ev.target);
-        }
-        console.log(ev);
-        e=ev;
-    };
-
-    this.copyToClip=function(){
+    this.copyToClip=function()
+    {
         var link = $(this).parents('.box').attr('link');
         navigator.clipboard.writeText(link);
     }
 
-    this.openInTab=function(){
+    this.openInTab=function()
+    {
         var link = $(this).parents('.box').attr('link');
+        var type = $(this).parents('.box').attr('type');
         window.open(link, '_blank');
     }
 
-    this.buildBoxes = function(arr){
+    this.buildBoxes = function(arr)
+    {
         var parent = $("#tabs");
         parent.empty();
         arr.forEach(function(val){
@@ -37,17 +34,16 @@ var home  = new function(){
                 temp = temp.replace(v["regex"],val[v["title"]]);
             });
             htmlObject = $(document.createElement('div'));
-            htmlObject.html(temp).addClass('box').attr('link',val['link']);
-            htmlObject.find('.linkVal').on('click',home.copyToClip);
+            htmlObject.html(temp).addClass('box').attr('link',val['link']).attr('type',val['type']);
             htmlObject.find('.info').on('click',home.openInTab);
             parent.append(htmlObject);
         });
     }
 
-    this.init = function(){
+    this.init = function()
+    {
         this.buildBoxes(this.arrAll);
     }
 
 }
-var e;
 home.init();
